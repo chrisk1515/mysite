980ef38cdc91e84f6b187bb961911afa9fac1d2c
Chris K <youremail@example.com>
2025-10-24 13:36:05 -1000

    init working dev server

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+﻿export default function Button({ children }: { children: React.ReactNode }) {
+  return (
+    <button className="px-4 py-2 rounded bg-black text-white">
+      {children}
+    </button>
+  );
+}
